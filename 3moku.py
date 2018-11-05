accbdea55722b76374d3c4db1b35ebb574170a6d
miki-minori <44741328+miki-minori@users.noreply.github.com>
2018-11-05 22:20:25 +0900

    ２Pプレイできるようにした

--- 3moku.py
+++ 3moku.py
@@ -11,75 +11,85 @@ def hyouji():
         print()
         print('---------')
 
-hyouji()
+def hantei(gamen,x):
+    if (gamen[0][0] == x) and (gamen[0][1] == x) and (gamen[0][2] == x):
+        return True
+    elif (gamen[1][0] == x) and (gamen[1][1] == x) and (gamen[1][2] == x):
+        return True
+    elif (gamen[2][0] == x) and (gamen[2][1] == x) and (gamen[2][2] == x):
+        return True
+    elif (gamen[0][0] == x) and (gamen[1][0] == x) and (gamen[2][0] == x):
+        return True
+    elif (gamen[0][1] == x) and (gamen[1][1] == x) and (gamen[2][1] == x):
+        return True
+    elif (gamen[0][2] == x) and (gamen[1][2] == x) and (gamen[2][2] == x):
+        return True
+    elif (gamen[0][0] == x) and (gamen[1][1] == x) and (gamen[2][2] == x):
+        return True
+    elif (gamen[0][2] == x) and (gamen[1][1] == x) and (gamen[2][1] == x):
+        return True
+    else:
+        return False
 
-for num in range(9):
-    if num %2 == 0:
-        line = input().split(" ")
-        n = int(line[0])
-        m = int(line[1])
-        while (n >= 3) or (m >= 3) or (gamen[n][m] != '＊'):
+print('mode select.')
+print('1人:0')
+print('2人:1')
+
+se = 10
+while (se != 0) or (se != 1):
+    se = int(input())
+    hyouji()
+    if se == 0:
+
+        for num in range(9):
+            if num %2 == 0:
+                line = input().split(" ")
+                n = int(line[0])
+                m = int(line[1])
+                while (n >= 3) or (m >= 3) or (gamen[n][m] != '＊'):
+                    line = input().split(" ")
+                    n = int(line[0])
+                    m = int(line[1])
+                gamen[n][m] = '〇'
+            else:
+                n = random.randint(0,2)
+                m = random.randint(0,2)
+                while (n >= 3) or (m >= 3) or (gamen[n][m] != '＊'):
+                    n = random.randint(0,2)
+                    m = random.randint(0,2)
+                gamen[n][m] = '✖'
+            hyouji()
+            print()
+
+            if hantei(gamen,'〇') == True:
+                print('〇の勝ち')
+                break
+            elif hantei(gamen,'✖') == True:
+                print('✖の勝ち')
+                break
+        print('終了')
+        break
+
+    elif se == 1:
+        for num in range(9):
             line = input().split(" ")
             n = int(line[0])
             m = int(line[1])
-        gamen[n][m] = '〇'
-    else:
-        n = random.randint(0,2)
-        m = random.randint(0,2)
-        while (n >= 3) or (m >= 3) or (gamen[n][m] != '＊'):
-            n = random.randint(0,2)
-            m = random.randint(0,2)
-        gamen[n][m] = '✖'
-    hyouji()
-    print()
+            while (n >= 3) or (m >= 3) or (gamen[n][m] != '＊'):
+                line = input().split(" ")
+                n = int(line[0])
+                m = int(line[1])
+            if num % 2 == 0:
+                gamen[n][m] = '〇'
+            else:
+                gamen[n][m] = '✖'
+            hyouji()
 
-    if (gamen[0][0] == '〇') and (gamen[0][1] == '〇') and (gamen[0][2] == '〇'):
-        print('〇の勝ち')
+            if hantei(gamen,'〇') == True:
+                print('〇の勝ち')
+                break
+            elif hantei(gamen,'✖') == True:
+                print('✖の勝ち')
+                break
+        print('終了')
         break
-    elif (gamen[0][0] == '✖') and (gamen[0][1] == '✖') and (gamen[0][2] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[1][0] == '〇') and (gamen[1][1] == '〇') and (gamen[1][2] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[1][0] == '✖') and (gamen[1][1] == '✖') and (gamen[1][2] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[2][0] == '〇') and (gamen[2][1] == '〇') and (gamen[2][2] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[2][0] == '✖') and (gamen[2][1] == '✖') and (gamen[2][2] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[0][0] == '〇') and (gamen[1][0] == '〇') and (gamen[2][0] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[0][0] == '✖') and (gamen[1][0] == '✖') and (gamen[2][0] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[0][1] == '〇') and (gamen[1][1] == '〇') and (gamen[2][1] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[0][1] == '✖') and (gamen[1][1] == '✖') and (gamen[2][1] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[0][2] == '〇') and (gamen[1][2] == '〇') and (gamen[2][2] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[0][2] == '✖') and (gamen[1][2] == '✖') and (gamen[2][2] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[0][0] == '〇') and (gamen[1][1] == '〇') and (gamen[2][2] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[0][0] == '✖') and (gamen[1][1] == '✖') and (gamen[2][2] == '✖'):
-        print('✖の勝ち')
-        break
-    elif (gamen[0][2] == '〇') and (gamen[1][1] == '〇') and (gamen[2][1] == '〇'):
-        print('〇の勝ち')
-        break
-    elif (gamen[0][2] == '✖') and (gamen[1][1] == '✖') and (gamen[2][1] == '✖'):
-        print('✖の勝ち')
-        break
-
-print('終了')
