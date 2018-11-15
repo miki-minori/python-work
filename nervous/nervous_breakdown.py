1b76c74b5af5a5cc0b05d8bd52db9e5051319a3a
miki-minori <44741328+miki-minori@users.noreply.github.com>
2018-11-15 19:22:12 +0900

    2人プレイできるようにした

--- nervous/nervous_breakdown.py
+++ nervous/nervous_breakdown.py
@@ -10,10 +10,25 @@ for h in range(2):
     print()
 
 print(place)
+player = [20,0,0]
+i = 0
 for num in range(100):
+    i += 1
+    if i == 1:
+        print('-------------------------------')
+        print('A者')
+        print(player[1],'枚')
+        print('-------------------------------')
+    elif i == 2:
+        print('-------------------------------')
+        print('B者')
+        print(player[2],'枚')
+        print('-------------------------------')
+    else:
+        i = 0
     select = [[99, 99], [99, 99]]
     for count in range(2):
-        while (select[count][0] > 1) or (select[count][1] > 9):
+        while (select[count][0] > 1) or (select[count][1] > 9) or (place[select[count][0]][select[count][1]] == 99):
             print()
             sel = input().split(" ")
             select[count][0] = int(sel[0])
@@ -35,3 +50,10 @@ for num in range(100):
     if (place[select[0][0]][select[0][1]]) == (place[select[1][0]][select[1][1]]):
         place[select[0][0]][select[0][1]] = 99
         place[select[1][0]][select[1][1]] = 99
+        player[i] += 2
+        player[0] -= 2
+        i -= 1
+
+    if player[0] == 0:
+        print('A者',player[1],'枚','B者',player[2],'枚')
+        break
