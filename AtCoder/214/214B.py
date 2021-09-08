180000584e52f63ea4e5b9f0330fd451a6d2fc90
DESKTOP-44A0UQ1\kymoh <kymohl246@gmail.com>
2021-09-08 22:53:33 +0900

    [add]ABC214_A問題, B問題

---
+++ AtCoder/214/214B.py
@@ -0,0 +1,10 @@
+S,T = map(int,input().split())
+count = 0
+
+for a in range(100):
+    for b in range(100):
+        for c in range(100):
+            if((a+b+c <= S) and (a*b*c <= T)):
+                count += 1
+            
+print(count)
