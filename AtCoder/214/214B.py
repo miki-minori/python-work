78a92d25b136fbdceb7776b937c7fe5deecc1123
DESKTOP-44A0UQ1\kymoh <kymohl246@gmail.com>
2021-09-15 22:26:01 +0900

    [fix]正常にカウントできるように修正

--- AtCoder/214/214B.py
+++ AtCoder/214/214B.py
@@ -1,10 +1,12 @@
 S,T = map(int,input().split())
 count = 0
 
-for a in range(100):
-    for b in range(100):
-        for c in range(100):
+for a in range(101):
+    for b in range(101):
+        for c in range(101):
             if((a+b+c <= S) and (a*b*c <= T)):
                 count += 1
-            
+            else:
+                break
+
 print(count)
